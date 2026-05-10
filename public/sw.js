1746eac57056f9610bc6e81f48a0ab8f5d1d060b
Yutojjj <100813506+Yutojjj@users.noreply.github.com>
2026-05-10 18:20:11 +0900

    a

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,11 @@
-// public/sw.js
 self.addEventListener('install', (e) => {
   console.log('[ServiceWorker] Installed');
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  // キャッシュはせず、常に最新のネットワークデータを取得する最小構成
+  // ネットワークを優先し、繋がらない時は簡易的なオフライン応答を返す
+  e.respondWith(
+    fetch(e.request).catch(() => new Response('アプリは現在オフラインですが起動可能です。'))
+  );
 });
